ed8d467604bb93c0da9139ccd1495a9f30cdcaa3
Deonte Kilgore <deonte.kilgore.dev@gmail.com>
2022-11-12 18:30:27 -0500

    Initial Commit

---
+++ ThemeSwitcher/ThemeSwitcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThemeSwitcherApp.swift
+//  ThemeSwitcher
+//
+//  Created by Deonte Kilgore on 11/12/22.
+//
+
+import SwiftUI
+
+@main
+struct ThemeSwitcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
