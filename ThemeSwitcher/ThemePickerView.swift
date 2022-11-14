e59a024c7d4fa2282ca6f4522a08534c39516d06
Deonte Kilgore <deonte.kilgore.dev@gmail.com>
2022-11-13 22:04:35 -0500

    Added a navigation view and title to the ThemePickerView.

--- ThemeSwitcher/ThemePickerView.swift
+++ ThemeSwitcher/ThemePickerView.swift
@@ -33,67 +33,72 @@ struct ThemePickerView: View {
     @Environment(\.dismiss) var dismiss
     
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("Plain")
-            HStack {
-                ForEach(plainThemes, id: \.color) { theme in
-                    Button {
-                        selectedTheme = theme
-                        dismiss()
-                    } label: {
-                        if let color = theme.color?.rawValue {
-                            Color(color)
-                                .frame(
-                                    width: self.selectedTheme == theme ? 100 : 50,
-                                    height: self.selectedTheme == theme ? 100 : 50
+        NavigationView {
+            VStack(alignment: .leading) {
+                Text("Plain")
+                HStack {
+                    ForEach(plainThemes, id: \.color) { theme in
+                        Button {
+                            selectedTheme = theme
+                            dismiss()
+                        } label: {
+                            if let color = theme.color?.rawValue {
+                                Color(color)
+                                    .frame(
+                                        width: self.selectedTheme == theme ? 100 : 80,
+                                        height: self.selectedTheme == theme ? 100 : 80
+                                    )
+                            }
+                        }
+                    }
+                }
+                .navigationTitle("Themes")
+                
+                Text("Gradients")
+                HStack {
+                    ForEach(gradientThemes, id: \.colors) { theme in
+                        if let colors = theme.colors {
+                            Button {
+                                selectedTheme = theme
+                                dismiss()
+                            } label: {
+                                LinearGradient(
+                                    colors: [Color(colors[0].rawValue), Color(colors[1].rawValue)],
+                                    startPoint: .top,
+                                    endPoint:.bottom
                                 )
+                                .frame(
+                                    width: self.selectedTheme == theme ? 100 : 80,
+                                    height: self.selectedTheme == theme ? 100 : 80
+                                )
+                            }
                         }
                     }
                 }
-            }
-            
-            Text("Gradients")
-            HStack {
-                ForEach(gradientThemes, id: \.colors) { theme in
-                    if let colors = theme.colors {
-                        Button {
-                            selectedTheme = theme
-                            dismiss()
-                        } label: {
-                            LinearGradient(
-                                colors: [Color(colors[0].rawValue), Color(colors[1].rawValue)],
-                                startPoint: .top,
-                                endPoint:.bottom
-                            )
-                            .frame(
-                                width: self.selectedTheme == theme ? 100 : 50,
-                                height: self.selectedTheme == theme ? 100 : 50
-                            )
+                
+                Text("Images")
+                HStack {
+                    ForEach(imageThemes, id: \.image) { theme in
+                        if let image = theme.image {
+                            Button {
+                                selectedTheme = theme
+                                dismiss()
+                            } label: {
+                                Image(image.rawValue)
+                                    .resizable()
+                                .frame(
+                                    width: self.selectedTheme == theme ? 100 : 80,
+                                    height: self.selectedTheme == theme ? 100 : 80
+                                )
+                            }
                         }
                     }
                 }
+                Spacer()
+
             }
-            
-            Text("Images")
-            HStack {
-                ForEach(imageThemes, id: \.image) { theme in
-                    if let image = theme.image {
-                        Button {
-                            selectedTheme = theme
-                            dismiss()
-                        } label: {
-                            Image(image.rawValue)
-                                .resizable()
-                            .frame(
-                                width: self.selectedTheme == theme ? 100 : 50,
-                                height: self.selectedTheme == theme ? 100 : 50
-                            )
-                        }
-                    }
-                }
-            }
+            .padding()
         }
-        .padding()
     }
 }
 
